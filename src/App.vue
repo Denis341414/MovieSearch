a2dd8461a7a957a0000c6482e74224922115c90d
Denis341414 <denismanshin2@gmail.com>
2024-06-26 20:15:17 +0300

    version1.3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script>
   import axios from 'axios'
-  import Search from './components/Search.vue'
-
+  
   export default {
-    components: {
-
-    },
+    
     data() {
       return {
         flagPopular: false,
         flagWatch: false,
+        flagStoped: false,
         info: '',
+        name: '',
+        posterThichElementUrl: '',
+        urlElement: '',
+        
       }
     },
     methods: {
@@ -21,10 +23,44 @@
         .then(res => {
           this.info = res.data
           console.log(this.info)
-          console.log(this.info[0].posterUrl)
-        }).catch(console.error('ddd'))
+        })
       },
-    }
+      getPlayersOnName() {
+        this.flagPopular = false
+        this.flagWatch = true
+        axios.get(`https://kinobox.tv/api/players?title=${this.name}`)
+        .then(res => {
+          console.log(res)
+          return res.data
+        })
+        .then(data => {
+          if (data[0].iframeUrl != null) this.urlElement = data[0].iframeUrl
+          else if (data[1].iframeUrl != null) this.urlElement = data[1].iframeUrl
+          else if (data[2].iframeUrl != null) this.urlElement = data[2].iframeUrl
+        })
+
+
+        axios.get('https://kinobox.tv/api/films/popular?type=film')
+        .then(result => {
+          return result.data
+        })
+        .then(data => {
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].title == this.name) {
+              this.posterThichElementUrl = data[i].posterUrl
+              this.flagPopular = false
+              this.flagWatch = false
+              this.flagStoped = true
+              console.log()
+            }
+          }
+        })
+
+      },
+      setNameFilms(el) {
+        this.name = el.title
+      }
+    },
   }
 </script>
 
@@ -32,7 +68,10 @@
   <header class="header">
     <div class="search">
         <h3>Search of FILMS</h3>
-        <Search/>
+        <div class="form1">
+          <input v-model="name" class="input" type="text" placeholder="search">
+          <button @click="getPlayersOnName" class="get">GET</button>
+        </div>
         <div class="form2">
           <button @click="getSourseApiPopular" class="popular">POPULAR</button>
         </div>
@@ -41,9 +80,22 @@
   <main class="main">
     <div v-if="flagPopular == true" class="cards">
       <div v-for="el in info" class="card">
-        <!-- <div :style="`background-image: url(${el.posterUrl})`" class="img">asdas</div> -->
         <p :style="`background-image: url(${el.posterUrl})`" class="name"></p>
-        <a href="#" class="watch">WATCH</a>
+        <a @click="setNameFilms(el)" href="#" class="watch">WATCH</a>
+      </div>
+    </div>
+
+    <div v-else-if="flagWatch == true" class="cards">
+      <div class="card">
+        <p :style="`background-image: url(${posterThichElementUrl})`" class="name"></p>
+        <a @click="" href="#" class="watch">WATCH</a>
+      </div>
+    </div>
+
+    <div v-else-if="flagStoped == true" class="cards">
+      <div class="card">
+        <p :style="`background-image: url(${posterThichElementUrl})`" class="name"></p>
+        <a @click="" :href="urlElement" class="watch">WATCH</a>
       </div>
     </div>
   </main> 
